fcd1869b69f5fc26285effaf88e850fc67879aad
Tao WANG15 王涛 <tao.wang15@nio.com>
2022-05-17 18:12:40 +0800

    Initial commit

---
+++ src/main/java/com/dd/nio/MagicTransformApplication.java
@@ -0,0 +1,62 @@
+package com.dd.nio;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.Context;
+import org.apache.tomcat.util.scan.StandardJarScanner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.TimeZone;
+
+/**
+ * Hello world!
+ *
+ */
+@Slf4j
+@SpringBootApplication
+@EnableAsync
+public class MagicTransformApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MagicTransformApplication.class);
+    }
+
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(MagicTransformApplication.class, args);
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application worksight is running! Access URLs:\n\t" +
+                "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+                "External: \thttp://" + ip + ":" + port + path + "/\n\t" +
+                "Swagger文档: \thttp://" + ip + ":" + port + path + "/doc.html\n" +
+                "电脑配置代理时Swagger文档: \thttp://localhost" + ":" + port + path + "/doc.html\n" +
+                "----------------------------------------------------------");
+    }
+
+    /**
+     * tomcat-embed-jasper引用后提示jar找不到的问题
+     */
+    @Bean
+    public TomcatServletWebServerFactory tomcatFactory() {
+        return new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                ((StandardJarScanner) context.getJarScanner()).setScanManifest(false);
+            }
+        };
+    }
+}
---
+++ src/main/java/com/dd/nio/config/mybatis/MybatisInterceptor.java
@@ -0,0 +1,104 @@
+package com.dd.nio.config.mybatis;
+
+import com.dd.nio.common.utils.oConvertUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.binding.MapperMethod.ParamMap;
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.SqlCommandType;
+import org.apache.ibatis.plugin.*;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Field;
+import java.util.Date;
+import java.util.Properties;
+
+/**
+ * mybatis拦截器，创建时间、修改时间
+ * @author tao.wang15
+ */
+@Slf4j
+@Component
+@Intercepts({ @Signature(type = Executor.class, method = "update", args = { MappedStatement.class, Object.class }) })
+public class MybatisInterceptor implements Interceptor {
+
+	@Override
+	public Object intercept(Invocation invocation) throws Throwable {
+		MappedStatement mappedStatement = (MappedStatement) invocation.getArgs()[0];
+		String sqlId = mappedStatement.getId();
+		log.debug("------sqlId------" + sqlId);
+		SqlCommandType sqlCommandType = mappedStatement.getSqlCommandType();
+		Object parameter = invocation.getArgs()[1];
+		log.debug("------sqlCommandType------" + sqlCommandType);
+
+		if (parameter == null) {
+			return invocation.proceed();
+		}
+		if (SqlCommandType.INSERT == sqlCommandType) {
+			Field[] fields = oConvertUtils.getAllFields(parameter);
+			for (Field field : fields) {
+				log.debug("------field.name------" + field.getName());
+				try {
+					// 注入创建时间
+					if ("createTime".equals(field.getName())) {
+						field.setAccessible(true);
+						Object local_createDate = field.get(parameter);
+						field.setAccessible(false);
+						if (local_createDate == null || local_createDate.equals("")) {
+							field.setAccessible(true);
+							field.set(parameter, new Date());
+							field.setAccessible(false);
+						}
+					}else if ("updateTime".equals(field.getName())) {
+						field.setAccessible(true);
+						field.set(parameter, new Date());
+						field.setAccessible(false);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		if (SqlCommandType.UPDATE == sqlCommandType) {
+			Field[] fields = null;
+			if (parameter instanceof ParamMap) {
+				ParamMap<?> p = (ParamMap<?>) parameter;
+				if (p.containsKey("et")) {
+					parameter = p.get("et");
+				} else {
+					parameter = p.get("param1");
+				}
+				if (parameter == null) {
+					return invocation.proceed();
+				}
+				fields = oConvertUtils.getAllFields(parameter);
+			} else {
+				fields = oConvertUtils.getAllFields(parameter);
+			}
+			for (Field field : fields) {
+				log.debug("------field.name------" + field.getName());
+				try {
+					if ("updateTime".equals(field.getName())) {
+						field.setAccessible(true);
+						field.set(parameter, new Date());
+						field.setAccessible(false);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return invocation.proceed();
+	}
+
+	@Override
+	public Object plugin(Object target) {
+		return Plugin.wrap(target, this);
+	}
+
+	@Override
+	public void setProperties(Properties properties) {
+		// TODO Auto-generated method stub
+	}
+
+}
